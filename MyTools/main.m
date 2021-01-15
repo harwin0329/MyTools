bb9c0b5ddc14dc0c06db10ba8e4a96f427a15a13
Harwin <chenzhiwen@51qub.com>
2021-01-15 09:42:16 +0800

    2021年01月15日09:42:07

---
+++ MyTools/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  MyTools
+//
+//  Created by Harwin on 2021/1/14.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello");
+        printf("MyTools");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        return 0;
+    }
+}
